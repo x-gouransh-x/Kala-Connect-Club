d393ce3d710ec0d40f08c92baeb993a5e9e3bf54
x-gouransh-x <gouranshahuja@gmail.com>
2025-08-14 02:00:06 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
